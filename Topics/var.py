2d06c23c4b574874cf398813593bd85d649eb3e6
reginatia07 <areginatia@gmail.com>
2026-03-19 22:19:03 +0000

    variables

---
+++ Topics/var.py
@@ -0,0 +1,12 @@
+# Avariable is reusable container for a value.
+# It is a named location in memory that can hold a value of a specific type.
+# Variables are used to store data that can be manipulated and accessed throughout a program.
+# the types of variables are: strings, integer, float and boolean.
+# there also different type or variable that is, multiple assignment
+
+name = "Reginatia"
+age = 17
+height = 1.65
+is_student = True
+
+print("My name is: {name}")
